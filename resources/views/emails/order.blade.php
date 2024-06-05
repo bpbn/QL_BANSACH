e08a49d63ecc4946b9b6c91098ca468916300c9b
Tuấn Việt IT <tuanvietcoder@gmail.com>
2024-06-05 19:54:11 +0700

    sendmail_update

---
+++ resources/views/emails/order.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h3>Xin chào : {{ $user->name }}</h3>
+    <p>
+        <b>Cám ơn bạn đã đặt hàng trên trang của chúng tôi !!! </b>
+        <p>Mã đơn hàng của bạn là : {{ $transaction->invoice_no }}</p>
+    </p>
+
+</div>
